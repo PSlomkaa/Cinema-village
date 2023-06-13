8d6c4ac031a5c9e062684c9fdfccd80202dd79a2
Piotr Słomka <114731942+PSlomkaa@users.noreply.github.com>
2023-06-13 08:29:32 +0200

    Add files via upload

---
+++ home/main.js
@@ -0,0 +1,28 @@
+// var swiper = new Swiper(".coming-container", {
+//     slidesPerView: 5,
+//     spaceBetween: 20,
+//     loop: true,
+//     centeredSlides: true,
+//     autoplay: {
+//         delay: 4000,
+//         disableOnInteraction: false,
+//     },
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//       },
+//     breakpoints: {
+//         0: {
+//             slidesPerView: 2,
+//         },
+//         568: {
+//             slidesPerView: 3,
+//         },
+//         786: {
+//             slidesPerView: 4,
+//         },
+//         986: {
+//             slidesPerView: 5,
+//         }
+//     }
+// })
